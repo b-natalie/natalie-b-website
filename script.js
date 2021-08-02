66eddce93ea751177c7b9c4cb15ac01cf2671965
Natalie Barba <nbarba2010@gmail.com>
2021-08-02 14:53:40 -0700

    Restructure to be about me

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 document.querySelector('body').appendChild(h2);
+
+const showDescription = document.getElementsByClassName('div');
+showDescription.addEventListener('hover', function(e) {
+    document.toggleClass('show-description');
+})
