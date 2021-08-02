77082893ddd4ad8bed74bf0cca5137d85c431434
Natalie Barba <nbarba2010@gmail.com>
2021-08-02 15:11:37 -0700

    Added click popup

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const inputSubmit = document.getElementById('submit');
+inputSubmit.addEventListener('click', function() {
+    alert('Success! I\'ll reach out within 2 days :)');
+});
+
+/*
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 document.querySelector('body').appendChild(h2);
-
-const showDescription = document.getElementsByClassName('div');
-showDescription.addEventListener('hover', function(e) {
-    document.toggleClass('show-description');
-})
+*/
